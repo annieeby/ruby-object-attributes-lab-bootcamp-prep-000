6b6fca0413984f5026e8da500f6a20870c13fd24
Annie Eby <eby.annie@gmail.com>
2018-10-10 23:54:04 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,23 +1,24 @@
 class Dog
+  
   def initialize(dog, breed)
     @name = name
     @breed = breed
   end
 
-def name=(name)         #reader / getter
-  @name = name
-end
-
-def breed=(breed)       #reader / getter
-  @breed = breed
-end
-
-def name                #writer / setter
-  "#{@name}".strip
-end
-
-def breed               #writer / setter
-  "#{@breed}".strip    
-end
+  def name=(name)         #reader / getter
+    @name = name
+  end
+  
+  def breed=(breed)       #reader / getter
+    @breed = breed
+  end
+  
+  def name                #writer / setter
+    "#{@name}".strip
+  end
+  
+  def breed               #writer / setter
+    "#{@breed}".strip    
+  end
 
 end
