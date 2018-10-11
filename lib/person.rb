87632924d6079bf45b1ebb65c0166fbcbae17c22
Annie Eby <eby.annie@gmail.com>
2018-10-11 00:03:49 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,2 +1,19 @@
 class Person
   
+  def name=(name)         #writer / setter  
+    @name = name
+  end
+  
+  def job=(job)       #writer / setter  
+    @job = job
+  end
+  
+  def name                #reader / getter
+    "#{@name}".strip
+  end
+  
+  def job               #reader / getter
+    "#{@job}".strip    
+  end
+
+end
